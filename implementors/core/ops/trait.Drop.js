8d723fafd1fa8bd926f3d0e5bf5d7fb426a13643
travis-ci <travis@travis-ci.org>
2014-10-14 09:04:15 +0000

    Auto doc upload from travis

---
+++ implementors/core/ops/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['zip'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;W: <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Writer.html' title='std::io::Writer'>Writer</a> + <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Seek.html' title='std::io::Seek'>Seek</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='zip/struct.ZipWriter.html' title='zip::ZipWriter'>ZipWriter</a>&lt;W&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
